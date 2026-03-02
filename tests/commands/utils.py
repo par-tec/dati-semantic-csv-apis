73f0add3d6084a8dd87add5df331d53ee8da6c45
Roberto Polli <robipolli@gmail.com>
2026-03-02 17:09:12 +0100

    Update snapshots.

--- tests/commands/utils.py
+++ tests/commands/utils.py
@@ -90,26 +90,58 @@ def assert_snapshot(fileinfo: dict):
     compare_f(snapshot_file, path)
 
 
-def compare_data(snapshot_file: Path, path: Path):
-    snapshot_data = yaml.safe_load(snapshot_file.read_text())
-    path_data = yaml.safe_load(path.read_text())
-    delta = DeepDiff(snapshot_data, path_data, ignore_order=True)
-    assert delta == {}
+def compare_data(snapshot_file: Path, current_file: Path):
+    """
+    Compare the data content of two files,
+    eventually retrieving the last committed version
+    from git.
+    """
+    current_data = yaml.safe_load(current_file.read_text())
+
+    if snapshot_file == current_file:
+        snapshot_raw: str = git_show_head(current_file)
+        snapshot_data = yaml.safe_load(snapshot_raw)
+    else:
+        snapshot_data = yaml.safe_load(snapshot_file.read_text())
+
+    delta = DeepDiff(snapshot_data, current_data, ignore_order=True)
+
+    assert not delta, (
+        f"File {current_file} differs from {snapshot_file}."
+        f" Either {current_file} is wrong,"
+        f" or {snapshot_file} has uncommitted changes."
+        f"\ndiff:\n{delta}"  # limit diff output to 500 chars
+    )
 
 
-def compare_content(snapshot_file: Path, path: Path):
-    if snapshot_file == path:
-        # compare the path file to its git commited version.
-        delta = git_diff(path)
+def compare_content(snapshot_file: Path, current_file: Path):
+    if snapshot_file == current_file:
+        # compare the current_file to its git commited version.
+        delta = git_diff(snapshot_file)
         assert not delta, (
-            f"File {path} has uncommitted changes. Please commit the file or update the snapshot reference."
+            f"File {snapshot_file} has uncommitted changes. Please commit the file or update the snapshot reference."
             f"\nGit diff:\n{delta.decode('utf-8')[:500]}"  # limit diff output to 500 chars
         )
     else:
         assert snapshot_file.exists(), (
             f"Expected snapshot file not found: {snapshot_file}"
         )
-        assert snapshot_file.read_bytes() == path.read_bytes()
+        assert snapshot_file.read_bytes() == current_file.read_bytes()
+
+
+def git_show_head(path: Path) -> str:
+    """
+    Get the git show of a file at HEAD as bytes.
+
+    :param path: Path to the file to get the show for
+    :return: The git show as bytes
+    """
+
+    repo = Repo(TESTDIR.parent, search_parent_directories=True)
+    show = repo.git.show(
+        f"HEAD:{path.as_posix()}",
+    )
+    return show
 
 
 def git_diff(path: Path) -> bytes:
